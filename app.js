ac059855bc0c511d6372c6c2ca66e5c388e012f1
jddev <hsd.salaheddine@gmail.com>
2022-10-26 00:01:42 +0100

    view engine setup

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 // Dependencies
 const express = require("express");
 const PORT = process.env.PORT || 3000;
+const path = require("path");
 
 const app = new express();
 
+//ejs
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-    res.end("Hello world!");
+    res.render("home");
 });
 
 app.listen(PORT, () => {
